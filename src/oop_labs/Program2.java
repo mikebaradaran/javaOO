9a21eb49d85af0c46c12c6fef4abc4f28c2d1328
mike Baradaran <mike.baradaran@qa.com>
2026-03-11 14:34:59 +0000

    Added the Zoo startup class

--- src/oop_labs/Program2.java
+++ src/oop_labs/Program2.java
@@ -6,15 +6,40 @@ public class Program2 {
 
 	public static void main(String[] args) {
 
-		List<String> friends = new ArrayList<>();
-		friends.add("Tom"); 			
-		friends.add("Sue"); 
-		friends.add("Sanjeev");
-		
-		for(String name : friends) {
-			System.out.println(name);
-		}
+		HashMap<String, Car> hm = new HashMap<>();
 
+		hm.put("Sam", new Car("Ford"));
+		hm.put("Joe", new Car("BMW"));
+
+		Car car = hm.get("Sam");
+		System.out.println(car.model);
+
+		car = hm.get("Joe");
+		System.out.println(car.model);
+		
+//		car = hm.get("Mike");
+//		if(car == null) {
+//			System.out.println("Unable to get Mike's car!");
+//		}
+		
+
+		if(hm.containsKey("Sam")) {
+			car = hm.get("Sam");
+			System.out.println( car.model);
+		}
+		else {
+			System.out.println("Unable to get the car!");
+		}
+		
 	}
 
+}
+
+
+class Car{
+	String model;
+	
+	public Car(String model) {
+		this.model = model;
+	}
 }
